642b05fd44c25af0b8a8f6672dff7cba6abdfaf2
Ramin Farkhondeh <ramin.farkhondeh@hotmail.com>
2023-06-25 18:38:26 +0200

    added new files to metrics __init__

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -2,4 +2,7 @@ from .dauc import *
 from .iauc import *
 from .ic import *
 from .dc import *
+from .sparsity import *
+from .selectivity import *
+from .sensitivity_n import *
 from .mutual_info import *
