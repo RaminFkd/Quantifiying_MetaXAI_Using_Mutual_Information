ffec332c02fd96ce1687f5d554ddbc90c12caa9f
Niklas Drews <niklasdrews@outlook.com>
2023-07-06 15:18:04 +0200

    added plot options

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -4,6 +4,50 @@ import pickle
 import traceback
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+
+def generate_color_palette(num_colors):
+    palette = []
+
+    # Generate a random hue value for the first color
+    hue = random.randint(0, 360)
+    palette.append(hue_to_rgb(hue))
+
+    # Generate the remaining colors
+    for _ in range(num_colors - 1):
+        # Randomly select a hue that is different from the previous hue
+        hue = (hue + random.randint(30, 90)) % 360
+        palette.append(hue_to_rgb(hue))
+
+    return palette
+
+def hue_to_rgb(hue):
+    # Convert HSL color to RGB color
+    saturation = random.uniform(0.4, 0.6)
+    lightness = random.uniform(0.4, 0.6)
+
+    c = (1 - abs(2 * lightness - 1)) * saturation
+    x = c * (1 - abs((hue / 60) % 2 - 1))
+    m = lightness - c / 2
+
+    if 0 <= hue < 60:
+        r, g, b = c, x, 0
+    elif 60 <= hue < 120:
+        r, g, b = x, c, 0
+    elif 120 <= hue < 180:
+        r, g, b = 0, c, x
+    elif 180 <= hue < 240:
+        r, g, b = 0, x, c
+    elif 240 <= hue < 300:
+        r, g, b = x, 0, c
+    else:
+        r, g, b = c, 0, x
+
+    r = float((r + m) )
+    g = float((g + m) )
+    b = float((b + m) )
+
+    return r, g, b
 
 
 def load_data(input_dir: str, extension: str = "pkl") -> dict:
@@ -70,15 +114,17 @@ def create_histogram(
         ylabel: str = "Frequency",
         bins="auto",
         output_path: str = None,
-        show: bool = False
+        show: bool = False,
+        color: str = None
 ):
-    # Create a random array of data
+    if not color:
+        color = generate_color_palette(1)[0]
 
     # Compute the histogram
     _, bins = np.histogram(data, bins=bins)
 
     # Plot the histogram
-    plt.hist(data, bins=bins)
+    plt.hist(data, bins=bins,color=color)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
@@ -97,13 +143,16 @@ def create_bar_chart(
         ylabel: str = "Mean values",
         tick_labels: list[str] = ['Data 1', 'Data 2', 'Data 3'],
         output_path: str = "output/bar_chart.png",
-        show: bool = False
+        show: bool = False,
+        color: str = None
 ):
     try:
+        if not color:
+            color = generate_color_palette(1) * len(data)
         fig, ax = plt.subplots()
         # Create the bar chart
         position = range(len(data))
-        bars = ax.bar(position, data, align='center')
+        bars = ax.bar(position, data, align='center',color=color)
         ax.bar_label(bars, fmt='%.2f')
         tick_labels = [f"({label[0]} , {label[1]})" for label in tick_labels]
         ax.set_xticks(position, tick_labels, rotation='vertical')
@@ -124,18 +173,19 @@ def create_bar_chart(
 
 
 def create_scatter(x: list[float], y: list[float, ], title: str = "Scatter Plot", x_label: str = "x", y_label: str = "y", output_path: str = None):
-
-    fig, ax = plt.subplots()
-    ax.scatter(x, y)
-    ax.set_title(title)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
-    if output_path:
-        plt.savefig(output_path, bbox_inches="tight")
-    else:
-        plt.show()
-    plt.close()
-
+    try:
+        fig, ax = plt.subplots()
+        ax.scatter(x, y)
+        ax.set_title(title)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        if output_path:
+            plt.savefig(output_path, bbox_inches="tight")
+        else:
+            plt.show()
+        plt.close()
+    except:
+        pass
 
 def create_bar(data: dict, sort: bool = False):
     for attribution, values in data.items():
@@ -218,10 +268,10 @@ def create_mean_bar(data: dict, sort: bool = False):
 def create_histograms(data: dict, bins: int = 10):
     for metric, values in data.items():
         save_path_hist = Path(
-            "output", "histograms", f"{metric[1]}_histogramm_{bins}.png")
+            "output", "histograms",f"{metric[0]}" ,f"{metric[1]}_histogramm_{bins}.png")
         save_path_hist.parent.mkdir(parents=True, exist_ok=True)
         create_histogram(
-            values[-1], title=f"{metric[0]} {metric[1]}", xlabel=f"{metric[1]} scores", ylabel="absolute Frequency", output_path=save_path_hist, bins=bins)
+            values[-1], title=f"{metric[0]} - {metric[1]}", xlabel=f"{metric[1]} scores", ylabel="absolute Frequency", output_path=save_path_hist, bins=bins)
 
 
 def create_scatter_plot(data: dict):
@@ -238,10 +288,13 @@ def create_scatter_plot(data: dict):
 
 
 if __name__ == "__main__":
-    in_dir = r"output/gradcam"
+    in_dir = r"output"
     data = load_mi(Path(r"data/mi.pkl"))
     create_bar(data, sort=False)
+    create_bar(data, sort=True)
+    create_mean_bar(data, sort=False)
     create_mean_bar(data, sort=True)
     data = load_data(in_dir)
     create_scatter_plot(data)
     create_histograms(data)
+    create_histograms(data,"auto")
